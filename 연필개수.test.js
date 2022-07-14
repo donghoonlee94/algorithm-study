12100add4c387c8bfaf4c5cc5eb13f0601a4d2dd
donghoonlee94 <94donghoon.lee@gmail.com>
2022-07-14 15:19:53 +0900

    add: 연필 개수 구하기

--- 연필개수.test.js
+++ 연필개수.test.js
@@ -1,3 +1,12 @@
-test('1 is 1', () => {
-  expect(1).toBe(1);
+// 연필 1다스는 12자루며, 학생 1인당 연필을 1자루씩 나누어 준다고 할 때 N명이 학생 수를 입력하면 필요한 연필의 다스 수를 계산하는 로직을 작성하세요.
+// 입력예제 : 25
+// 출력 예제 : 3
+
+function solution(n) {
+  return Math.ceil(n / 12);
+}
+
+test('연필 개수', () => {
+  expect(solution(25)).toBe(3);
+  expect(solution(178)).toBe(15);
 });
